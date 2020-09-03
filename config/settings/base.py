db2e9e24955b9a793d09e9759452930318a43bdc
shunnyjang <suuuu101@naver.com>
2020-09-04 02:59:49 +0900

    Fix invalid payload

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
 JWT_AUTH = {
     'JWT_ENCODE_HANDELR': 'rest_framework_jwt.utils.jwt_encode_handler',
     'JWT_DECODE_HANDLER': 'rest_framework_jwt.utils.jwt_decode_handler',
-    'JWT_PAYLOAD_HANDLER': 'config.token.jwt_payload_handler',
+    'JWT_PAYLOAD_HANDLER': 'config.token.my_jwt_payload_handler',
     'JWT_PAYLOAD_GET_USER_ID_HANDLER': 'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
     'JWT_RESPONSE_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_response_payload_handler',
     
--- config/token.py
+++ config/token.py
@@ -1,17 +1,7 @@
-from datetime import datetime
-from calendar import timegm
-from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.utils import jwt_payload_handler
 
-def jwt_payload_handler(user):
-    """ Custom payload handler
-    Token encrypts the dictionary returned by this function, and can be decoded by rest_framework_jwt.utils.jwt_decode_handler
-    """
-    return {
-        'user_pk': user.pk,
-        'userID': user.userID,
-        'user_role': user.role,
-        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA,
-        'orig_iat': timegm(
-            datetime.utcnow().utctimetuple()
-        )
-    }
+def my_jwt_payload_handler(user):
+    payload = jwt_payload_handler(user)
+    payload['user_role'] = user.role
+
+    return payload

0b90877e8183e89aeec3212c8f49b52398180ced
shunnyjang <suuuu101@naver.com>
2020-09-04 05:48:36 +0900

    Fix error of obtain_jwt_token
    jwt.view.obtain_jwt_token은 일부 경우에 정상적으로 토큰을 발행하지 못함

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('signup/', views.UserCreateView.as_view(), name='signup'),
     path('id/', views.CheckIdView.as_view(), name="check_id"),
     path('auth/', views.PhoneAuthView.as_view(), name="phone_auth"),
-    path('login/', obtain_jwt_token),
+    path('login/', views.LoginJWTView.as_view(), name="login"),
     path('shelter/', views.ShelterCreateView.as_view(), name="create_shelter"),
     path('shelter/<int:pk>/', views.ShelterDetailView.as_view(), name="detail_shelter"),
 ]

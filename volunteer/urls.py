d21c2a8793ea03b996c9d9b82eed5d2d86fe9615
shunnyjang <suuuu101@naver.com>
2020-09-05 23:06:44 +0900

    Delete unused url

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostView.as_view(), name='post'),
-    path('post/', views.PostCreateView.as_view(), name='post-create'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('status/', views.VolunteerRetrieveView.as_view(), name='volunteer_status'),
     path('apply/', views.VolunteerApplyView.as_view(), name='volunteer_apply'),

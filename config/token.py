444797fbd8aef6e715bbca18aa2e3613191c9f09
shunnyjang <suuuu101@naver.com>
2020-09-10 23:58:56 +0900

    Add shelter id for shelter staff user

--- config/token.py
+++ config/token.py
@@ -3,5 +3,6 @@ from rest_framework_jwt.utils import jwt_payload_handler
 def my_jwt_payload_handler(user):
     payload = jwt_payload_handler(user)
     payload['user_role'] = user.role
-
+    if user.role == "2":
+        payload['shelter'] = user.shelter.id
     return payload

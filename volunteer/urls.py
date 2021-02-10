4ef36e4215af64ea16f2e4edd0f54f35b210a9ba
shunnyjang <suuuu101@naver.com>
2021-02-11 02:09:16 +0900

    Fix to remove as_view() from function based API view

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -5,7 +5,7 @@ from .APIs import applyment, recruitment
 urlpatterns = [
     path('', recruitment.RecruitmentView.as_view(), name='recruitment_list'),
     path('<int:pk>/', recruitment.RecruitmentDetailView.as_view(), name='recruitment_detail'),
-    path('status/', applyment.list_of_volunteer_for_shelter.as_view(), name='volunteer_status'),
+    path('status/', applyment.list_of_volunteer_for_shelter, name='volunteer_status'),
     path('apply/', applyment.VolunteerApplyView.as_view(), name='volunteer_apply'),
-    path('list/', applyment.list_of_applying_volunteer_of_user.as_view(), name='applying_volunteer_list'),
+    path('list/', applyment.list_of_applying_volunteer_of_user, name='applying_volunteer_list'),
 ]

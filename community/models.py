9e8a75a575c2b8109d850ac170124dc2b68c04a1
shunnyjang <suuuu101@naver.com>
2021-02-11 02:12:02 +0900

    Clean code by PEP8

--- community/models.py
+++ community/models.py
@@ -7,11 +7,13 @@ from uuid import uuid4
 from django.db import models
 from django.utils import timezone
 
+
 def date_upload_to(instance, filename):
     ymd_path = timezone.now().strftime('%Y/%m/%d')
     uuid_name = uuid4().hex
     extension = os.path.splitext(filename)[-1].lower()
     return '/'.join([ymd_path, uuid_name + extension, ])
+
 
 class Community(models.Model):
     shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE)
@@ -19,10 +21,12 @@ class Community(models.Model):
     image = models.ImageField(blank=True, upload_to=date_upload_to)
     content = models.TextField(blank=True)
 
+
 class Charity(models.Model):
     shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=True)
+
 
 class CharityImage(models.Model):
     charity = models.ForeignKey(Charity, on_delete=models.CASCADE)
